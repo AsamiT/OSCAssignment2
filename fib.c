2dbd64668efe8d478062d04fc615066561e88499
Asami Tachibana <atachibana2612@gmail.com>
2018-11-05 12:37:03 -0500

    Fibonacci works!

--- fib.c
+++ fib.c
@@ -18,7 +18,10 @@ int result;
 int numPass;
 double *p;
 
-/* declare fibonacci function for threading. */
+/* This function performs the fibonacci sequencing. */
+/* First, it declares the value of "integer n" from the value passed to the function through pthread.
+ Once this is done, it declares the constants we know of in the fibonacci sequence--0 and 1. Once that is done, it iterates up to the value of n and performs the required fibonacci calculations therein. */
+
 void *fib(void *vargp) {
 	int n = (int *)vargp; //set value of n to argument passed from main.
 	for (int i = 0; i <= n; i++) { //from i=0 to i=n, iterate.
@@ -32,53 +35,59 @@ void *fib(void *vargp) {
 		}
 		if (i >= 2) {
 			result = n1 + n2; //now calculate F(2) to F(n).
-			*(p + i) = result;
-			n2 = n1;
+			*(p + i) = result; //push this value to the array.
+			n2 = n1; /* Here, we're moving everything down a spell. */
 			n1 = result;
 		}
 	}
-	printf("\n"); //print newline to clean up
-	return n;
+	printf("\n"); //print newline. This keeps the output clean.
+    return n; //return the n value back to the main function.
 }
 
 int main(int argc, char *argv[]) {
 
 	if (!argv[1]) {
-		printf("fib syntax: int value between 3 and 30\n"); //we're arbitrarily definining a cap on how far you can go at 30. around f(50) or so we'd have to use a larger integer space, but I'd rather not fuss with that.
+		printf("fib syntax: int value between 3 and 46\n");
+        /*
+            This is being arbitrarily capped to 46, because trying to create a system
+         that allows for fibonacci sequences which exceed 32-bit integer values is not something
+         I wish to relive. I've already done this before, and it took more than the time allotted to me
+         by this assignment. For the sake of performing threading, 46 positions is more than enough to demonstrate the sequencing. */
+         
 		return -1;
 	}
 
 	numPass = atoi(argv[1]);
 
-	int arr[numPass];
-	p = arr;
+	double arr[numPass]; //this declares our array
+	p = arr; //this tells the pointer to point to arr[0]. This is how we're going to access it from all threads.
 
 	if (!numPass) { //validation
-		printf("Please enter an integer value between 3 and 30.\n");
+		printf("Please enter an integer value between 3 and 46.\n");
 		return 0;
 	}
-	if (numPass < 3 || numPass > 30) { //validation
-		printf("Please enter an integer value between 3 and 30.\n");
+	if (numPass < 3 || numPass > 46) { //validation
+		printf("Please enter an integer value between 3 and 46.\n");
 		return 0;
 	}
 
 
 	/* The following code will:
-		- Generate thread
-		- Pass variable to thread and execute function void fib()
-		- Join thread. 
+		- Generate thread identifier
+		- Pass variable to thread and execute function void fib() within a new thread,
+		- Join thread (so to execute the fibonacci sequencing),
 		- (DEBUG) Print to indicate that the parent must execute after the child.
-	
+        - Iterate through the double array to print all the fibonacci numbers we have in sequence.
+        - Exit program gracefully.
 	*/
+    
 	pthread_t threadIdent;
 	pthread_create(&threadIdent, NULL, fib, numPass);
 	pthread_join(threadIdent, NULL);
-	//printf("This should have happened after the thread is done executing.\n");
 
 	for (int z = 0; z <= numPass; z++) {
 		int arrayVal = *(p + z);
 		printf("F(%d): %d\n", z, arrayVal);
 	}
-
-	exit(0);
+	exit(0); //exit program.
 }
